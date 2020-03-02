a1bc3d3dff0d0478d4f6234c738514e17d3e0c7c
eamoses <e.a.moses@gmail.com>
2020-03-01 19:43:42 -0600

    making data dynamic

--- map-timelapse/js/onload.js
+++ map-timelapse/js/onload.js
@@ -2,9 +2,10 @@ window.onload = function(){
   console.log('oh, hi!')
 }
 
+const date = '02-27';
 const fetchMe = document.getElementById('fetch-button');
 fetchMe.addEventListener('click', function(){
-    fetch('http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-01-2020.csv')
+    fetch('http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+date+'-2020.csv')
     .then(function(response) {
       return response.text();
     })
