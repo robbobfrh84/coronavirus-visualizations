08f425c873db7e3e0e117829a51e2747d9994220
Bob Main <bobmain49@gmail.com>
2020-03-01 21:18:42 -0600

    adding delay for plots test

--- map-timelapse/js/buildMap.js
+++ map-timelapse/js/buildMap.js
@@ -1,5 +1,9 @@
 // https://docs.mapbox.com/mapbox-gl-js/example/add-image-animated/
-const buildMap = function() {
+const buildMap = function(objKey, locations) {
+
+  console.log(objKey)
+  const dates = [1,2,3,4,5,6].splice(2,4)
+  console.log("dates :", dates)
 
 	mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iYm9iZnJoODQiLCJhIjoiY2pvamcyNXUzMDFiMDNwcnc2Z2dibm10ZCJ9.y7ll2wHKfb5WIwtAtK9eJA';
     var map = new mapboxgl.Map({
@@ -7,37 +11,26 @@ const buildMap = function() {
         style: 'mapbox://styles/mapbox/streets-v9',
         zoom: 0.5,
         center: [150, 20]
-    });
-
-    var size = 50;
-
-    // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
-    // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
+    })
+    var size = 25
     var pulsingDot = {
         width: size,
         height: size,
         data: new Uint8Array(size * size * 4),
-
-        // get rendering context for the map canvas when layer is added to the map
         onAdd: function() {
-            var canvas = document.createElement('canvas');
-            canvas.width = this.width;
-            canvas.height = this.height;
-            this.context = canvas.getContext('2d');
+            var canvas = document.createElement('canvas')
+            canvas.width = this.width
+            canvas.height = this.height
+            this.context = canvas.getContext('2d')
         },
-
-        // called once before every frame where the icon will be used
         render: function() {
             var duration = 1000;
-            var t = (performance.now() % duration) / duration;
-
-            var radius = (size / 2) * 0.3;
-            var outerRadius = (size / 2) * 0.7 * t + radius;
-            var context = this.context;
-
-            // draw outer circle
-            context.clearRect(0, 0, this.width, this.height);
-            context.beginPath();
+            var t = (performance.now() % duration) / duration
+            var radius = (size / 2) * 0.3
+            var outerRadius = (size / 2) * 0.7 * t + radius
+            var context = this.context
+            context.clearRect(0, 0, this.width, this.height)
+            context.beginPath()
             context.arc(
                 this.width / 2,
                 this.height / 2,
@@ -45,11 +38,9 @@ const buildMap = function() {
                 0,
                 Math.PI * 2
             );
-            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-            context.fill();
-
-            // draw inner circle
-            context.beginPath();
+            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')'
+            context.fill()
+            context.beginPath()
             context.arc(
                 this.width / 2,
                 this.height / 2,
@@ -57,56 +48,60 @@ const buildMap = function() {
                 0,
                 Math.PI * 2
             );
-            context.fillStyle = 'rgba(255, 100, 100, 1)';
-            context.strokeStyle = 'white';
-            context.lineWidth = 2 + 4 * (1 - t);
-            context.fill();
-            context.stroke();
-
-            // update this image's data with data from the canvas
+            context.fillStyle = 'rgba(255, 100, 100, 1)'
+            context.strokeStyle = 'white'
+            context.lineWidth = 2 + 4 * (1 - t)
+            context.fill()
+            context.stroke()
             this.data = context.getImageData(
                 0,
                 0,
                 this.width,
                 this.height
             ).data;
-
-            // continuously repaint the map, resulting in the smooth animation of the dot
-            map.triggerRepaint();
-
-            // return `true` to let the map know that the image was updated
-            return true;
+            map.triggerRepaint()
+            return true
         }
-    };
+    }
 
     map.on('load', function() {
-        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
 
-        map.addSource('points', {
-            'type': 'geojson',
-            'data': {
-                'type': 'FeatureCollection',
-                'features': [
-                    {
-                        'type': 'Feature',
-                        'geometry': {
-                            'type': 'Point',
-                            'coordinates': [0, 0]
-                        }
-                    }
-                ]
-            }
-        });
-        map.addLayer({
-            'id': 'points',
-            'type': 'symbol',
-            'source': 'points',
-            'layout': {
-                'icon-image': 'pulsing-dot'
-            }
-        });
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 })
 
+        let cnt = 0
+        setTimeout(function(){ place(117.2264, 31.8257) }, 1000)
+        setTimeout(function(){ place(116.4142, 40.1824) }, 2000)
+        setTimeout(function(){ place(107.874, 30.0572) }, 3000)
+        setTimeout(function(){ place(117.9874, 26.0789) }, 4000)
+        setTimeout(function(){ place(103.8343, 36.0611) }, 5000)
 
-    });
+        const place = function(lng, lat){
+          cnt++
+          map.addLayer({
+              'id': 'points'+cnt,
+              'type': 'symbol',
+              'source': {
+                  'type': 'geojson',
+                  'data': {
+                      'type': 'FeatureCollection',
+                      'features': [
+                          {
+                              'type': 'Feature',
+                              'geometry': {
+                                  'type': 'Point',
+                                  'coordinates': [lng, lat]
+                              }
+                          }
+                      ]
+                  }
+              },
+              'layout': {
+                  'icon-image': 'pulsing-dot',
+                  "icon-allow-overlap": true,
+              }
+          })
+        }
+
+    })
 
 }
--- map-timelapse/js/onload.js
+++ map-timelapse/js/onload.js
@@ -5,16 +5,18 @@ window.onload = function(){
 
 const fetchMe = document.getElementById('fetch-button');
 fetchMe.addEventListener('click', function(){
-    fetch('http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-01-2020.csv')
+    fetch('http://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv')
     .then(function(response) {
       return response.text();
     })
     .then(function(data) {
         const jsonResponse = JSON.stringify(data)
         const fetchIt = document.getElementById('fetchIt');
-        fetchIt.innerHTML = `
-        Fetch Response:
-            ${data}
-        `
+        console.log(data)
+        //console.log()
+        // fetchIt.innerHTML = `
+        // Fetch Response:
+        //     ${data}
+        // `
     });
 });
